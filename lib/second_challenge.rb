441218dd3e78a4ebec249b185e158198a8d345a0
Sean Padden <sean.padden7@gmail.com>
2019-05-10 20:54:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch: {}
+      :patriarch => {}
       :matriarch => {}
       :hero => {}
       :hero_friends => []
       }, 
     :capulet => {
-      patriarch: {}
+      :patriarch => {}
       :matriarch => {}
       :heroine => {}
       :heroine_friends => []
