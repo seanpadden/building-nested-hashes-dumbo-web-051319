244b856aa3fcc0dcf15608c57e657c506c3f468b
Sean Padden <sean.padden7@gmail.com>
2019-05-10 21:06:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
