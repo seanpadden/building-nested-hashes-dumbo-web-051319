479e5ecd9b3a23a03ec87cbdac0bf64549a20e85
Sean Padden <sean.padden7@gmail.com>
2019-05-10 21:01:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,8 +6,8 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        name: "Lady Montague",
-        age: "54"
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
