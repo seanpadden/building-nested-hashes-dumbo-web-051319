c98e03cd28a875377bc6dc6abb155043c2576f0f
Sean Padden <sean.padden7@gmail.com>
2019-05-10 21:05:42 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,32 +3,32 @@ def third_challenge
    :montague => {
       :patriarch => {
         name: "Lord Montague",
-        :age => "53"
+        age: "53"
         },
       :matriarch => {
         name: "Lady Montague",
-        :age => "54"
+        age: "54"
         },
       :hero => {
         name: "Romeo",
-        :age => "15",
-        :status => "alive"
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         :name: "Lord Capulet"
-        :age => "50"
+        age: "50"
         },
       :matriarch => {
         name: "Lady Capulet"
-        :age => "51"
+        age: "51"
         },
       :heroine => {
         name: "Juliet",
         age: "15",
-        :status => "alive"
+        status: "alive"
         },
       :heroine_friends => []
    }
